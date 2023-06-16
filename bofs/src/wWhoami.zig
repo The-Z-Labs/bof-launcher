35d09fd44cee02da11d0cee053aea3250205cc8f
Michal Ziulek <michalziulek@gmail.com>
2023-06-16 13:54:04 +0200

    bofs: Added 'wWhoami' BOF (wip).

---
+++ bofs/src/wWhoami.zig
@@ -0,0 +1,19 @@
+const w32 = @import("bofapi").win32;
+const beacon = @import("bofapi").beacon;
+
+fn getTokenInfo(token_type: w32.TOKEN_INFORMATION_CLASS) ?*anyopaque {
+    _ = token_type;
+    var token: w32.HANDLE = undefined;
+    if (w32.OpenProcessToken(w32.GetCurrentProcess(), w32.TOKEN_READ, &token) == w32.TRUE) {
+        _ = beacon.printf(
+            0,
+            "TOKEN OPENED!\n",
+        );
+    }
+    return null;
+}
+
+pub export fn go(_: ?[*]u8, _: i32) callconv(.C) u8 {
+    _ = getTokenInfo(.TokenUser);
+    return 0;
+}
