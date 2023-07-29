43e17a293277314db86f2845639dd62e8fe4158e
mzet <mzet@owasp.org>
2023-07-29 14:03:53 +0200

    cli4bofs: add usage documentation

--- examples/cli4bofs/README.md
+++ examples/cli4bofs/README.md
@@ -4,14 +4,30 @@ Command line interface for BOFs.
 
 ## Description
 
+Universal tool that allows to execute any BOF from filesystem and pass arguments to it. Handy for testing, prototyping and developing BOFs.
+
 ## Usage
 
-    cli4bofs_win_x64 <BOF> <BOF args>
-    cli4bofs_win_x86 <BOF> <BOF args>
-    cli4bofs_lin_x64 <BOF> <BOF args>
-    cli4bofs_lin_x86 <BOF> <BOF args>
+```
+Usage: ./zig-out/bin/cli4bofs_lin_x64 <BOF> [[prefix:]ARGUMENT]...
 
-For example:
+Execute given BOF from filesystem with provided ARGUMENTs.
 
-    cli4bofs_win_x64.exe wWinver.coff.x64.o
-    cli4bofs_lin_x64 cUDPscan.elf.x64.o 162.159.200.1-5:123,88
+ARGUMENTS:
+
+ARGUMENT's data type can be specified using one of following prefix:
+	short OR s	 - 16-bit signed integer.
+	int OR i	 - 32-bit signed integer.
+	str OR z	 - zero-terminated characters string.
+	wstr OR Z	 - zer-terminated wide characters string.
+	file OR b	 - special type followed by file path indicating that a pointer to a buffer filled with content of the file will be passed to BOF.
+
+If prefix is ommited then ARGUMENT is treated as a zero-terminated characters string (str / z).
+
+EXAMPLES:
+
+cli4bofs uname -a
+cli4bofs udpScanner 192.168.2.2-10:427
+cli4bofs udpScanner z:192.168.2.2-10:427
+cli4bofs udpScanner 192.168.2.2-10:427 file:/path/to/file/with/udpPayloads
+```
--- examples/cli4bofs/src/main.zig
+++ examples/cli4bofs/src/main.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const assert = std.debug.assert;
 const bof = @import("bofapi").bof;
 
+const stdout = std.io.getStdOut();
+
 fn runBofFromFile(
     allocator: std.mem.Allocator,
     bof_path: [:0]const u8,
@@ -27,8 +29,21 @@ fn runBofFromFile(
 }
 
 fn usage(name: [:0]const u8) void {
-    const stdout = std.io.getStdOut();
-    stdout.writer().print("Usage: {s} <bof>\n", .{name}) catch unreachable;
+    stdout.writer().print("Usage: {s} <BOF> [[prefix:]ARGUMENT]...\n\n", .{name}) catch unreachable;
+    stdout.writer().print("Execute given BOF from filesystem with provided ARGUMENTs.\n\n", .{}) catch unreachable;
+    stdout.writer().print("ARGUMENTS:\n\n", .{}) catch unreachable;
+    stdout.writer().print("ARGUMENT's data type can be specified using one of following prefix:\n", .{}) catch unreachable;
+    stdout.writer().print("\tshort OR s\t - 16-bit signed integer.\n", .{}) catch unreachable;
+    stdout.writer().print("\tint OR i\t - 32-bit signed integer.\n", .{}) catch unreachable;
+    stdout.writer().print("\tstr OR z\t - zero-terminated characters string.\n", .{}) catch unreachable;
+    stdout.writer().print("\twstr OR Z\t - zer-terminated wide characters string.\n", .{}) catch unreachable;
+    stdout.writer().print("\tfile OR b\t - special type followed by file path indicating that a pointer to a buffer filled with content of the file will be passed to BOF.\n", .{}) catch unreachable;
+    stdout.writer().print("\nIf prefix is ommited then ARGUMENT is treated as a zero-terminated characters string (str / z).\n", .{}) catch unreachable;
+    stdout.writer().print("\nEXAMPLES:\n\n", .{}) catch unreachable;
+    stdout.writer().print("cli4bofs uname -a\n", .{}) catch unreachable;
+    stdout.writer().print("cli4bofs udpScanner 192.168.2.2-10:427\n", .{}) catch unreachable;
+    stdout.writer().print("cli4bofs udpScanner z:192.168.2.2-10:427\n", .{}) catch unreachable;
+    stdout.writer().print("cli4bofs udpScanner 192.168.2.2-10:427 file:/path/to/file/with/udpPayloads\n", .{}) catch unreachable;
 }
 
 pub fn main() !u8 {
@@ -47,14 +62,16 @@ pub fn main() !u8 {
 
     const prog_name = iter.next() orelse unreachable;
     const bof_name = iter.next() orelse {
+        stdout.writer().print("No BOF provided. Aborting.\n\n", .{}) catch unreachable;
         usage(prog_name);
-        return error.BofError;
+        return 0;
     };
 
     var bof_path_buffer: [std.fs.MAX_PATH_BYTES:0]u8 = undefined;
     const absolute_bof_path = std.fs.cwd().realpathZ(bof_name, bof_path_buffer[0..]) catch {
+        stdout.writer().print("BOF not found. Aborting.\n\n", .{}) catch unreachable;
         usage(prog_name);
-        return error.BofError;
+        return 0;
     };
     bof_path_buffer[absolute_bof_path.len] = 0;
 
