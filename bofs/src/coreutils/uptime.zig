ab69107bbdc76a3b16956c27f39f6e46f35f8878
mzet <mzet@owasp.org>
2023-11-07 11:29:30 +0100

    bofs/coreutils: update

--- bofs/src/coreutils/uptime.zig
+++ bofs/src/coreutils/uptime.zig
@@ -3,11 +3,14 @@ const beacon = @import("bofapi").beacon;
 const unix = @import("bofapi").unix;
 
 pub export fn go() callconv(.C) u8 {
-    _ = beacon.printf(0, "uptime BOF\n");
-
     unix.setutxent();
 
-    _ = unix.puts("Just testing.\n");
+    var ut: ?*unix.utmpx = unix.getutxent();
+
+    if (ut) |utx| {
+        _ = beacon.printf(0, "User %s\n", utx.ut_user);
+        _ = beacon.printf(0, "Type %d\n", utx.ut_type);
+    }
 
     unix.endutxent();
     return 0;
