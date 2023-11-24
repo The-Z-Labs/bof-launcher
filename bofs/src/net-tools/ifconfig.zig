56e77f223dc02d85f5164356334064187be40390
mzet <mzet@owasp.org>
2023-11-24 14:58:53 +0100

    'ifconfig' BOF: update

--- bofs/src/net-tools/ifconfig.zig
+++ bofs/src/net-tools/ifconfig.zig
@@ -1,22 +1,44 @@
 const std = @import("std");
+const os = @import("std").os;
+const c = @import("std").c;
 const beacon = @import("bofapi").beacon;
 
+pub const ifaddrs = extern struct {
+    ifa_next: *ifaddrs,
+    ifa_name: [*:0]u8,
+    ifa_flags: u32,
+    ifa_addr: *os.sockaddr,
+    ifa_netmask: *os.sockaddr,
+    pub const ifa_ifu = extern union {
+        ifu_broadaddr: *os.sockaddr,
+        ifu_dstaddr: *os.sockaddr,
+    };
+};
+
+//https://man7.org/linux/man-pages/man3/getifaddrs.3.html
+pub extern fn getifaddrs(ifap: *[*]ifaddrs) callconv(.C) i32;
+pub extern fn freeifaddrs(ifap: [*]ifaddrs) callconv(.C) void;
+
 pub export fn go(args: ?[*]u8, args_len: i32) callconv(.C) u8 {
-    if (args_len == 0) {
+    var ifap: [*]ifaddrs = undefined;
+
+    if (args_len != 0) {
+        _ = beacon.printf(0, "Interface was provided.\n");
+    }
+
+    _ = args;
+
+    if (getifaddrs(&ifap) == -1) {
+        _ = beacon.printf(0, "getifaddrs failed. Aborted.\n");
         return 1;
     }
 
-    var opt_size: i32 = 0;
-
-    var parser = beacon.datap{};
-
-    beacon.dataParse(&parser, args, args_len);
-    const opt = beacon.dataExtract(&parser, &opt_size);
-    const optS = opt.?[0..@as(usize, @intCast(opt_size - 1))];
-
-    if (std.mem.eql(u8, optS, "-a")) {
-        _ = beacon.printf(0, "ifconfig\n");
+    var ifi: ?*ifaddrs = &ifap[0];
+    while (ifi != null) : (ifi = ifi.?.ifa_next) {
+        _ = beacon.printf(0, "if name: %s.\n", ifi.?.ifa_name);
     }
+
+    freeifaddrs(ifap);
 
     return 0;
 }
