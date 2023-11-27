cc637529ffe7f793f78bb2b0685ab418de121854
michal-z <michalziulek@gmail.com>
2023-11-27 18:44:20 +0100

    Call BeaconPrintf() from asm on Linux as an example.

--- bofs/src/lAsmTest.asm
+++ bofs/src/lAsmTest.asm
@@ -1,14 +1,23 @@
 format ELF64
 
 public go
+extrn 'BeaconPrintf' as BeaconPrintf:qword
 
 section '.text' executable
 
 align 8
 go:
+    push rbp
+    mov eax, 12345
+
+    mov edi, 0
+    lea rsi, [msg]
+    mov edx, eax
+    call BeaconPrintf
+
     xor eax, eax
+    pop rbp
     ret
 
-section '.data' writeable
-
-;msg db "Elves are coming!",0xA,0
+align 8
+msg db "Hello from asm BOF on Linux! eax is %d", 0xa, 0
