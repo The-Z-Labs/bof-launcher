f0a2744e4cee31183ad03d4e9cfddb816633aa52
Michal Ziulek <michalziulek@gmail.com>
2023-12-06 11:08:33 +0100

    build: Added Zig version check.

--- build.zig
+++ build.zig
@@ -1,5 +1,7 @@
 const std = @import("std");
 
+pub const min_zig_version = std.SemanticVersion{ .major = 0, .minor = 12, .patch = 0, .pre = "dev.1769" };
+
 pub const Options = struct {
     target: std.zig.CrossTarget,
     optimize: std.builtin.Mode,
@@ -32,6 +34,8 @@ pub const Options = struct {
 };
 
 pub fn build(b: *std.build.Builder) void {
+    ensureZigVersion() catch return;
+
     const optimize = b.option(
         std.builtin.Mode,
         "optimize",
@@ -247,4 +251,27 @@ pub fn build(b: *std.build.Builder) void {
 
 inline fn thisDir() []const u8 {
     return comptime std.fs.path.dirname(@src().file) orelse ".";
+}
+
+fn ensureZigVersion() !void {
+    var installed_ver = @import("builtin").zig_version;
+    installed_ver.build = null;
+
+    if (installed_ver.order(min_zig_version) == .lt) {
+        std.log.err("\n" ++
+            \\---------------------------------------------------------------------------
+            \\
+            \\Installed Zig compiler version is too old.
+            \\
+            \\Min. required version: {any}
+            \\Installed version: {any}
+            \\
+            \\Please install newer version and try again.
+            \\Latest version can be found here: https://ziglang.org/download/
+            \\
+            \\---------------------------------------------------------------------------
+            \\
+        , .{ min_zig_version, installed_ver });
+        return error.ZigIsTooOld;
+    }
 }
