aef94599475b669207ea17781ba5b18c403b6c55
michal-z <michalziulek@gmail.com>
2024-01-10 14:50:05 +0100

    Test cleanups.

--- build.zig
+++ build.zig
@@ -63,24 +63,7 @@ pub fn build(b: *std.Build) void {
         //
         // Run test BOFs (`zig build test`)
         //
-        if (options.target.result.cpu.arch == @import("builtin").cpu.arch and
-            options.target.result.os.tag == @import("builtin").os.tag)
-        {
-            test_step.dependOn(&@import("tests/build.zig").runTests(
-                b,
-                options,
-                bof_launcher_lib,
-                bof_launcher_api_module,
-                bof_api_module,
-            ).step);
-        }
-
-        // TODO: Zig bug? Error in the test runner on Linux (tests pass but memory error is reported).
-        if (@import("builtin").os.tag == .linux and options.target.result.cpu.arch == .x86) continue;
-
-        if (options.target.result.cpu.arch == .x86 and @import("builtin").cpu.arch == .x86_64 and
-            options.target.result.os.tag == @import("builtin").os.tag)
-        {
+        if (options.target.result.os.tag == @import("builtin").os.tag) {
             test_step.dependOn(&@import("tests/build.zig").runTests(
                 b,
                 options,
@@ -95,61 +78,6 @@ pub fn build(b: *std.Build) void {
     // BOFs
     //
     @import("bofs/build.zig").build(b, bof_api_module);
-
-    //
-    // Additional Linux tests
-    //
-    // TODO: Move below tests to `test.zig`
-    if (false and @import("builtin").os.tag == .linux and @import("builtin").cpu.arch == .x86_64) {
-        const run_qemu_tests = b.option(bool, "qemu", "Run aarch64 and arm qemu tests") orelse false;
-
-        if (run_qemu_tests) {
-            // Try to run on aarch64 using qemu
-            const udp_scanner_aarch64 = b.addSystemCommand(&.{
-                "qemu-aarch64",
-                "-L",
-                "/usr/aarch64-linux-gnu",
-                "zig-out/bin/cli4bofs_lin_aarch64",
-                "zig-out/bin/udpScanner.elf.aarch64.o",
-                "192.168.0.1:2-10",
-            });
-            udp_scanner_aarch64.step.dependOn(b.getInstallStep());
-
-            const test_obj0_aarch64 = b.addSystemCommand(&.{
-                "qemu-aarch64",
-                "-L",
-                "/usr/aarch64-linux-gnu",
-                "zig-out/bin/cli4bofs_lin_aarch64",
-                "zig-out/bin/test_obj0.elf.aarch64.o",
-            });
-            test_obj0_aarch64.step.dependOn(b.getInstallStep());
-
-            // Try to run on arm using qemu
-            const udp_scanner_arm = b.addSystemCommand(&.{
-                "qemu-arm",
-                "-L",
-                "/usr/arm-linux-gnueabihf",
-                "zig-out/bin/cli4bofs_lin_arm",
-                "zig-out/bin/udpScanner.elf.arm.o",
-                "192.168.0.1:2-10",
-            });
-            udp_scanner_arm.step.dependOn(b.getInstallStep());
-
-            const test_obj0_arm = b.addSystemCommand(&.{
-                "qemu-arm",
-                "-L",
-                "/usr/arm-linux-gnueabihf",
-                "zig-out/bin/cli4bofs_lin_arm",
-                "zig-out/bin/test_obj0.elf.arm.o",
-            });
-            test_obj0_arm.step.dependOn(b.getInstallStep());
-
-            test_step.dependOn(&udp_scanner_aarch64.step);
-            test_step.dependOn(&udp_scanner_arm.step);
-            test_step.dependOn(&test_obj0_aarch64.step);
-            test_step.dependOn(&test_obj0_arm.step);
-        }
-    }
 }
 
 inline fn thisDir() []const u8 {
