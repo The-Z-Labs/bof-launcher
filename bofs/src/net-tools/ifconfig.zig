27f69455fa972ffcc615ff85197f6a2348d1ddf3
mzet <mzet@z-labs.eu>
2024-04-09 18:15:22 +0200

    bofs: net-tools/ifconfig BOF update

--- bofs/src/net-tools/ifconfig.zig
+++ bofs/src/net-tools/ifconfig.zig
@@ -4,41 +4,44 @@ const c = @import("std").c;
 const beacon = @import("bof_api").beacon;
 
 pub const ifaddrs = extern struct {
-    ifa_next: *ifaddrs,
-    ifa_name: [*:0]u8,
+    ifa_next: ?*ifaddrs,
+    ifa_name: ?[*:0]u8,
     ifa_flags: u32,
-    ifa_addr: *posix.sockaddr,
-    ifa_netmask: *posix.sockaddr,
-    pub const ifa_ifu = extern union {
-        ifu_broadaddr: *posix.sockaddr,
-        ifu_dstaddr: *posix.sockaddr,
-    };
+    ifa_addr: ?*posix.sockaddr,
+    ifa_netmask: ?*posix.sockaddr,
+    ifa_ifu: extern union {
+        ifu_broadaddr: ?*posix.sockaddr,
+        ifu_dstaddr: ?*posix.sockaddr,
+    },
+    ifa_data: ?*anyopaque,
 };
 
 //https://man7.org/linux/man-pages/man3/getifaddrs.3.html
-pub extern fn getifaddrs(ifap: *[*]ifaddrs) callconv(.C) i32;
-pub extern fn freeifaddrs(ifap: [*]ifaddrs) callconv(.C) void;
+pub extern fn getifaddrs(ifap: **ifaddrs) callconv(.C) i32;
+pub extern fn freeifaddrs(ifap: *ifaddrs) callconv(.C) void;
 
 pub export fn go(args: ?[*]u8, args_len: i32) callconv(.C) u8 {
-    var ifap: [*]ifaddrs = undefined;
+    _ = .{ args, args_len };
 
-    if (args_len != 0) {
-        _ = beacon.printf(0, "Interface was provided.\n");
-    }
-
-    _ = args;
-
-    if (getifaddrs(&ifap) == -1) {
+    var family: i32 = undefined;
+    var list: *ifaddrs = undefined;
+    if (getifaddrs(&list) == -1) {
         _ = beacon.printf(0, "getifaddrs failed. Aborted.\n");
         return 1;
     }
 
-    var ifi: ?*ifaddrs = &ifap[0];
-    while (ifi != null) : (ifi = ifi.?.ifa_next) {
-        _ = beacon.printf(0, "if name: %s.\n", ifi.?.ifa_name);
+    var iter: ?*ifaddrs = list;
+    while (iter != null) : (iter = iter.?.ifa_next) {
+        family = iter.?.ifa_addr.?.family;
+        _ = beacon.printf(0, "if name: %s ", iter.?.ifa_name);
+        //if(family == std.os.linux.AF.PACKET) {
+        //    _ = beacon.printf(0, "AF_PACKET", .{});
+        //}
+    
+	//std.debug.print("{any}\n\n", .{iter.?.*});
     }
 
-    freeifaddrs(ifap);
+    freeifaddrs(list);
 
     return 0;
 }
