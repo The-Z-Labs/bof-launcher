e463a445bd90cf998d7363f8ee730bcfc4379873
mzet <mzet@owasp.org>
2024-04-16 16:18:33 +0200

    bofs/net-tools/ifconfig: display the same data and in similar format as 'ifconfig' tool

--- bofs/src/net-tools/ifconfig.zig
+++ bofs/src/net-tools/ifconfig.zig
@@ -1,10 +1,35 @@
+// TODO:
+// get MAC address
+// get MTU value
 const std = @import("std");
 const posix = @import("std").posix;
 const c = @import("std").c;
 const beacon = @import("bof_api").beacon;
 
+// https://github.com/ziglang/zig/blob/a2b834e8c7152f70d71c71107db40b9182909647/lib/libc/include/generic-glibc/netdb.h
 const NI_MAXHOST = 1025;
 const NI_NUMERICHOST = 1;
+
+// https://github.com/ziglang/zig/blob/a2b834e8c7152f70d71c71107db40b9182909647/lib/libc/musl/include/net/if.h
+pub const IFF_UP = 0x1;
+pub const IFF_BROADCAST = 0x2;
+pub const IFF_DEBUG = 0x4;
+pub const IFF_LOOPBACK = 0x8;
+pub const IFF_POINTOPOINT = 0x10;
+pub const IFF_NOTRAILERS = 0x20;
+pub const IFF_RUNNING = 0x40;
+pub const IFF_NOARP = 0x80;
+pub const IFF_PROMISC = 0x100;
+pub const IFF_ALLMULTI = 0x200;
+pub const IFF_MASTER = 0x400;
+pub const IFF_SLAVE = 0x800;
+pub const IFF_MULTICAST = 0x1000;
+pub const IFF_PORTSEL = 0x2000;
+pub const IFF_AUTOMEDIA = 0x4000;
+pub const IFF_DYNAMIC = 0x8000;
+pub const IFF_LOWER_UP = 0x10000;
+pub const IFF_DORMANT = 0x20000;
+pub const IFF_ECHO = 0x40000;
 
 pub const ifaddrs = extern struct {
     ifa_next: ?*ifaddrs,
@@ -33,12 +58,29 @@ pub extern fn getnameinfo(addr: *posix.sockaddr,
     flags: u32,
 ) callconv(.C) c.EAI;
 
+fn flagsDisplay(flags: u32) void {
+    _ = beacon.printf(0, "flags=%u<", flags);
+
+    if(flags & IFF_UP != 0)
+        _ = beacon.printf(0, "UP");
+    if(flags & IFF_BROADCAST != 0)
+        _ = beacon.printf(0, ",BROADCAST");
+    if(flags & IFF_LOOPBACK != 0)
+        _ = beacon.printf(0, ",LOOPBACK");
+    if(flags & IFF_POINTOPOINT != 0)
+        _ = beacon.printf(0, ",POINT-TO-POINT");
+    if(flags & IFF_RUNNING != 0)
+        _ = beacon.printf(0, ",RUNNING");
+    if(flags & IFF_MULTICAST != 0)
+        _ = beacon.printf(0, ",MULTICAST");
+    _ = beacon.printf(0, ">");
+}
+
 pub export fn go(args: ?[*]u8, args_len: i32) callconv(.C) u8 {
     _ = .{ args, args_len };
 
     const allocator = std.heap.page_allocator;
 
-    var family: i32 = undefined;
     var list: *ifaddrs = undefined;
 
     if (getifaddrs(&list) == -1) {
@@ -64,30 +106,48 @@ pub export fn go(args: ?[*]u8, args_len: i32) callconv(.C) u8 {
 
     // iterate over each interface name and print its statistics
     for (interfaces.items) |iface| {
-        _ = beacon.printf(0, "%s: flags=4163<UP,BROADCAST,RUNNING,MULTICAST>  mtu 1500\n", iface.ptr);
 
         iter = list;
         while (iter != null) : (iter = iter.?.ifa_next) {
             var host = [_]u8{0} ** NI_MAXHOST;
-            var host6 = [_]u8{0} ** NI_MAXHOST;
-	    family = iter.?.ifa_addr.?.family;
+            var netmask = [_]u8{0} ** NI_MAXHOST;
+            var aux = [_]u8{0} ** NI_MAXHOST;
+	    const family = iter.?.ifa_addr.?.family;
             const cur_iface_name = std.mem.sliceTo(iter.?.ifa_name.?, 0);
+            const flags = iter.?.ifa_flags;
 
             if(std.mem.eql(u8, iface, cur_iface_name)) {
+
                 if (family == std.os.linux.AF.INET) {
                     _ = getnameinfo(iter.?.ifa_addr.?, @sizeOf(std.posix.sockaddr.in), &host, NI_MAXHOST, null, 0, NI_NUMERICHOST);
-		    _ = beacon.printf(0, "       inet %s netmask=\n", &host);
+                    _ = getnameinfo(iter.?.ifa_netmask.?, @sizeOf(std.posix.sockaddr.in), &netmask, NI_MAXHOST, null, 0, NI_NUMERICHOST);
+
+                    if(flags & IFF_BROADCAST != 0) {
+                        _ = getnameinfo(iter.?.ifa_ifu.ifu_broadaddr.?, @sizeOf(std.posix.sockaddr.in), &aux, NI_MAXHOST, null, 0, NI_NUMERICHOST);
+		        _ = beacon.printf(0, "\tinet %s netmask=%s broadcast=%s\n", &host, &netmask, &aux);
+                    }
+                    else {
+                        _ = getnameinfo(iter.?.ifa_ifu.ifu_dstaddr.?, @sizeOf(std.posix.sockaddr.in), &aux, NI_MAXHOST, null, 0, NI_NUMERICHOST);
+		        _ = beacon.printf(0, "\tinet %s netmask=%s point2point=%s\n", &host, &netmask, &aux);
+                    }
                 }
                 if (family == std.os.linux.AF.INET6) {
-                    _ = getnameinfo(iter.?.ifa_addr.?, @sizeOf(std.posix.sockaddr.in6), &host6, NI_MAXHOST, null, 0, NI_NUMERICHOST);
-		    _ = beacon.printf(0, "       inet6 %s netmask=\n", &host6);
+                    _ = getnameinfo(iter.?.ifa_addr.?, @sizeOf(std.posix.sockaddr.in6), &host, NI_MAXHOST, null, 0, NI_NUMERICHOST);
+                    _ = getnameinfo(iter.?.ifa_netmask.?, @sizeOf(std.posix.sockaddr.in6), &netmask, NI_MAXHOST, null, 0, NI_NUMERICHOST);
+		    _ = beacon.printf(0, "\tinet %s netmask=%s\n", &host, &netmask);
                 }
 	        if (family == std.os.linux.AF.PACKET) {
 
+                    _ = beacon.printf(0, "%s: ", iface.ptr);
+                    flagsDisplay(flags);
+                    _ = beacon.printf(0, "\n");
+
 	            if(iter.?.ifa_data != null) {
                         const stats = @as(*std.os.linux.rtnl_link_stats, @ptrCast(@alignCast(iter.?.ifa_data)));
-		        _ = beacon.printf(0, "       RX packets %d bytes %d\n", stats.rx_packets, stats.rx_bytes);
-		        _ = beacon.printf(0, "       TX packets %d bytes %d\n", stats.tx_packets, stats.tx_bytes);
+		        _ = beacon.printf(0, "\tRX packets %d bytes %d\n", stats.rx_packets, stats.rx_bytes);
+		        _ = beacon.printf(0, "\tRX errors %d dropped %d overruns %d frame %d\n", stats.rx_errors, stats.rx_dropped, stats.rx_fifo_errors, stats.rx_frame_errors);
+		        _ = beacon.printf(0, "\tTX packets %d bytes %d\n", stats.tx_packets, stats.tx_bytes);
+		        _ = beacon.printf(0, "\tTX errors %d dropped %d overruns %d carrier %d collisions %d\n", stats.tx_errors, stats.tx_dropped, stats.tx_fifo_errors, stats.tx_carrier_errors, stats.collisions);
                     }
                 }
             }
