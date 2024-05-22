d59f25193c3c86b5f0daf1e85a29303de04aa2f7
Michal Ziulek <michalziulek@gmail.com>
2024-05-22 12:58:53 +0200

    wCloneProcess BOF: Set Size field in PS_CREATE_INFO.

--- bofs/src/wCloneProcess.zig
+++ bofs/src/wCloneProcess.zig
@@ -4,8 +4,10 @@ const w32 = @import("bof_api").win32;
 pub export fn go(_: ?[*]u8, _: i32) callconv(.C) u8 {
     var process_handle: w32.HANDLE = undefined;
     var thread_handle: w32.HANDLE = undefined;
+
     var create_info: w32.PS_CREATE_INFO = undefined;
     @memset(std.mem.asBytes(&create_info), 0);
+    create_info.Size = @sizeOf(w32.PS_CREATE_INFO);
 
     const nt_status = w32.NtCreateUserProcess(
         &process_handle,
