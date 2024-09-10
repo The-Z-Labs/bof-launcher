3484f94bad22897cde1c3ad7cab17afebf8f54aa
mzet <mzet@owasp.org>
2024-09-10 13:11:05 +0200

    integration-with-c: make.bat fixes

--- examples/integration-with-c/make.bat
+++ examples/integration-with-c/make.bat
@@ -1,12 +1,8 @@
 @echo off
 
 set CC=zig cc
-set CFLAGS=-I../../include -I../../bof-launcher/src -std=c99 -fno-sanitize=undefined -c
+set CFLAGS=-I../../bof-launcher/src -std=c99 -fno-sanitize=undefined -c
 
-%CC% %CFLAGS% ../../bof-launcher/src/beacon/stb_sprintf.c
-%CC% %CFLAGS% ../../bof-launcher/src/beacon/beacon_impl.c
 %CC% %CFLAGS% main.c
 
-zig build-obj -O ReleaseSmall -fstrip ../../bof-launcher/src/bof_launcher.zig
-
-%CC% -o bof_launcher.exe main.obj bof_launcher.obj beacon_impl.obj stb_sprintf.obj ole32.lib ws2_32.lib
+%CC% -o bof_launcher.exe ../../zig-out/lib/bof_launcher_win_x64.lib main.obj ole32.lib ws2_32.lib
