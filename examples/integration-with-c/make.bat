4c46704d6105df488e8791adeed0b27ba7b43160
Michal Ziulek <michalziulek@gmail.com>
2024-09-10 14:18:25 +0200

    integration-with-c: Fixed make.bat

--- examples/integration-with-c/make.bat
+++ examples/integration-with-c/make.bat
@@ -5,4 +5,4 @@ set CFLAGS=-I../../bof-launcher/src -std=c99 -fno-sanitize=undefined -c
 
 %CC% %CFLAGS% main.c
 
-%CC% -o bof_launcher.exe ../../zig-out/lib/bof_launcher_win_x64.lib main.obj ole32.lib ws2_32.lib
+%CC% -o bof_launcher.exe ../../zig-out/lib/bof_launcher_win_x64.lib main.obj -lole32 -lws2_32
