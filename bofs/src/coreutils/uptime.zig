9448c747c981cfcc1dc1007af05d82d538bdcf4c
mzet <mzet@owasp.org>
2025-06-26 00:32:54 +0200

    BOFs: 'uptime' BOF update (WIP)

--- bofs/src/coreutils/uptime.zig
+++ bofs/src/coreutils/uptime.zig
@@ -18,91 +18,6 @@ const BofErrors = enum(u8) {
     UnknownError,
 };
 
-// RFC3339 implementation taken from:
-// https://www.aolium.com/karlseguin/cf03dee6-90e1-85ac-8442-cf9e6c11602a
-pub const DateTime = struct {
-  year: u16,
-  month: u8,
-  day: u8,
-  hour: u8,
-  minute: u8,
-  second: u8,
-};
-
-pub fn fromTimestamp(ts: u64) DateTime {
-  const SECONDS_PER_DAY = 86400;
-  const DAYS_PER_YEAR = 365;
-  const DAYS_IN_4YEARS = 1461;
-  const DAYS_IN_100YEARS = 36524;
-  const DAYS_IN_400YEARS = 146097;
-  const DAYS_BEFORE_EPOCH = 719468;
-
-  const seconds_since_midnight: u64 = @rem(ts, SECONDS_PER_DAY);
-  var day_n: u64 = DAYS_BEFORE_EPOCH + ts / SECONDS_PER_DAY;
-  var temp: u64 = 0;
-
-  temp = 4 * (day_n + DAYS_IN_100YEARS + 1) / DAYS_IN_400YEARS - 1;
-  var year: u16 = @intCast(100 * temp);
-  day_n -= DAYS_IN_100YEARS * temp + temp / 4;
-
-  temp = 4 * (day_n + DAYS_PER_YEAR + 1) / DAYS_IN_4YEARS - 1;
-  year += @intCast(temp);
-  day_n -= DAYS_PER_YEAR * temp + temp / 4;
-
-  var month: u8 = @intCast((5 * day_n + 2) / 153);
-  const day: u8 = @intCast(day_n - (@as(u64, @intCast(month)) * 153 + 2) / 5 + 1);
-
-  month += 3;
-  if (month > 12) {
-    month -= 12;
-    year += 1;
-  }
-
-  return DateTime{
-    .year = year,
-    .month = month,
-    .day = day,
-    .hour = @intCast(seconds_since_midnight / 3600),
-    .minute = @intCast(seconds_since_midnight % 3600 / 60),
-    .second = @intCast(seconds_since_midnight % 60)
-  };
-}
-
-pub fn toRFC3339(dt: DateTime) [20]u8 {
-  var buf: [20]u8 = undefined;
-  _ = std.fmt.formatIntBuf(buf[0..4], dt.year, 10, .lower, .{.width = 4, .fill = '0'});
-  buf[4] = '-';
-  paddingTwoDigits(buf[5..7], dt.month);
-  buf[7] = '-';
-  paddingTwoDigits(buf[8..10], dt.day);
-  buf[10] = ' ';
-
-  paddingTwoDigits(buf[11..13], dt.hour);
-  buf[13] = ':';
-  paddingTwoDigits(buf[14..16], dt.minute);
-  buf[16] = ':';
-  paddingTwoDigits(buf[17..19], dt.second);
-  buf[19] = 0;
-
-  return buf;
-}
-
-fn paddingTwoDigits(buf: *[2]u8, value: u8) void {
-  switch (value) {
-    0 => buf.* = "00".*,
-    1 => buf.* = "01".*,
-    2 => buf.* = "02".*,
-    3 => buf.* = "03".*,
-    4 => buf.* = "04".*,
-    5 => buf.* = "05".*, 
-    6 => buf.* = "06".*,
-    7 => buf.* = "07".*,
-    8 => buf.* = "08".*,
-    9 => buf.* = "09".*,
-    else => _ = std.fmt.formatIntBuf(buf, value, 10, .lower, .{}),
-  }
-}
-// end of RFC3339 implementation
 
 fn getUptimeLinux() !u8 {
     var buffer = [_]u8{0} ** 100;
@@ -110,27 +25,32 @@ fn getUptimeLinux() !u8 {
     const f = try std.fs.openFileAbsoluteZ("/proc/uptime", .{ .mode = .read_only} );
     defer f.close();
 
-    _ = try f.reader().readUntilDelimiterOrEof(&buffer, '\n') orelse
+    const uptimeStr = try f.reader().readUntilDelimiterOrEof(&buffer, '.') orelse
         return @intFromEnum(BofErrors.UnknownError);
 
-    var iter = std.mem.splitScalar(u8, &buffer, ' ');
-    const upt = iter.next() orelse return @intFromEnum(BofErrors.UnknownError);
-    const idlet = iter.next() orelse return @intFromEnum(BofErrors.UnknownError);
+    const SECONDS_PER_DAY = 86400;
+    const uptimeSec = try std.fmt.parseInt(u64, uptimeStr, 10); 
 
-    const uptime = @as(u64, @intFromFloat(try std.fmt.parseFloat(f64, upt)));
-    _ = beacon.printf(0, "%s %d\n", &buffer, uptime);
+    _ = beacon.printf(0, "up: ");
 
-    const dt = fromTimestamp(uptime);
-    const timeStr = toRFC3339(dt);
-    _ = beacon.printf(0, "%s", &timeStr);
+    const days: u8 = @intCast(uptimeSec / SECONDS_PER_DAY);
+    const hours: u8 = @intCast(uptimeSec % SECONDS_PER_DAY / 3600);
+    const minutes: u8 = @intCast(uptimeSec % SECONDS_PER_DAY % 3600 / 60);
 
-    const idletime = @as(u64, @intFromFloat(try std.fmt.parseFloat(f64, idlet)));
-    _ = beacon.printf(0, "%s %d\n", &buffer, idletime);
+    if(days > 0) {
+        _ = beacon.printf(0, "%d ", days);
+        if(days == 1) { _ = beacon.printf(0, "day "); } else _ = beacon.printf(0, "days ");
+    }
+    if(hours > 0) {
+        _ = beacon.printf(0, "%d ", hours);
+        if(hours == 1) { _ = beacon.printf(0, "hour "); } else _ = beacon.printf(0, "hours ");
+    }
+    if(minutes > 0) {
+        _ = beacon.printf(0, "%d ", minutes);
+        if(minutes == 1) { _ = beacon.printf(0, "minute "); } else _ = beacon.printf(0, "minutes");
+    }
 
-    const dt2 = fromTimestamp(idletime);
-    const timeStr2 = toRFC3339(dt2);
-    _ = beacon.printf(0, "%s", &timeStr2);
-
+    _ = beacon.printf(0, "\n");
     return 0;
 }
 
