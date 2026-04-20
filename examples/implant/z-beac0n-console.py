c76dec0e9969bbc717d3b557a1e0a9386d9eac22
mzet <mzet@owasp.org>
2026-04-20 09:22:04 +0200

    work on z-beac0n console has started

---
+++ examples/implant/z-beac0n-console.py
@@ -0,0 +1,16 @@
+
+print(
+"""
+             bb                             00000          
+zzzzz        bb        eee    aa aa   cccc 00   00 nn nnn  
+  zz  _____  bbbbbb  ee   e  aa aaa cc     00   00 nnn  nn 
+ zz          bb   bb eeeee  aa  aaa cc     00   00 nn   nn 
+zzzzz        bbbbbb   eeeee  aaa aa  ccccc  00000  nn   nn 
+                                                           
+""")
+
+prompt = "z-beac0n>"
+
+while True:
+    print(prompt, end='')
+    input()
