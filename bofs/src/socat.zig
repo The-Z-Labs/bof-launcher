4ff0bae036be0e0bfa6f8e339e7555be6ecacdfa
mzet <mzet@owasp.org>
2026-05-22 11:33:32 +0200

    bofs/socat: update

--- bofs/src/socat.zig
+++ bofs/src/socat.zig
@@ -13,11 +13,36 @@ comptime {
 // BOF-specific error codes
 const BofErrors = enum(u8) {
     AccesDenied = 0x1,
-    NoArgsProvided = 0x2,
-    CwdUnlinked,
-    NameTooLong,
+    NoArgsProvided,
+    BadArgsProvided,
+    NotSupportedAddressType,
+    ConnectionError,
+    DataTransferError,
     UnknownError,
 };
+
+const AddressType = enum(u8) {
+    OPEN = 0x1,
+    CREATE,
+    STDIN,
+    TCP,
+    TLS,
+    UNRECOGNIZED,
+};
+
+fn checkAddressType(addr_type: []const u8) AddressType {
+    if(std.mem.eql(u8, "OPEN", addr_type)) {
+        return AddressType.OPEN;
+    } else if(std.mem.eql(u8, "CREATE", addr_type)) {
+        return AddressType.CREATE;
+    } else if(std.mem.eql(u8, "TCP", addr_type)) {
+        return AddressType.TCP;
+    } else if(std.mem.eql(u8, "TLS", addr_type)) {
+        return AddressType.TLS;
+    }
+    else
+        return AddressType.UNRECOGNIZED;
+}
 
 pub export fn go(adata: ?[*]u8, alen: i32) callconv(.c) u8 {
     @import("bof_api").init(adata, alen, .{});
@@ -31,31 +56,53 @@ pub export fn go(adata: ?[*]u8, alen: i32) callconv(.c) u8 {
     var parser = beacon.datap{};
     beacon.dataParse(&parser, adata, alen);
 
-    if (beacon.dataExtract(&parser, null)) |address1| {
+    var srcAddrType: AddressType = undefined;
+    var src_addr_iter: std.mem.SplitIterator(u8, .scalar) = undefined;
 
-        var iter = std.mem.splitScalar(u8, std.mem.sliceTo(address1, 0), ':');
+    var sinkAddrType: AddressType = undefined;
+    var sink_addr_iter: std.mem.SplitIterator(u8, .scalar) = undefined;
 
-        const file_path = iter.next() orelse return 1;
-        const file = std.fs.openFileAbsolute(file_path, .{}) catch return 1;
+    const src_address = std.mem.sliceTo(beacon.dataExtract(&parser, null).?, 0);
+    const sink_address = std.mem.sliceTo(beacon.dataExtract(&parser, null).?, 0);
+
+    if (!std.mem.eql(u8, "", src_address)) {
+
+        if(std.mem.eql(u8, "-", std.mem.sliceTo(src_address, 0)))
+            srcAddrType = AddressType.STDIN;
+
+        src_addr_iter = std.mem.splitScalar(u8, std.mem.sliceTo(src_address, 0), ':');
+        const addrPrefix = src_addr_iter.next() orelse return @intFromEnum(BofErrors.BadArgsProvided);
+
+        srcAddrType = checkAddressType(addrPrefix);
+        if (srcAddrType != AddressType.OPEN)
+            return @intFromEnum(BofErrors.NotSupportedAddressType);
+
+        const file_path = src_addr_iter.next() orelse return @intFromEnum(BofErrors.BadArgsProvided);
+        const file = std.fs.openFileAbsolute(file_path, .{ .mode = .read_only }) catch return 1;
 
         var recv_buffer: [4096]u8 = undefined;
         var file_r = file.reader(&recv_buffer);
         const file_r_iface = &file_r.interface;
 
-        if (beacon.dataExtract(&parser, null)) |address2| {
+        if (!std.mem.eql(u8, "", sink_address)) {
 
-            var iter2 = std.mem.splitScalar(u8, std.mem.sliceTo(address2, 0), ':');
+            sink_addr_iter = std.mem.splitScalar(u8, std.mem.sliceTo(sink_address, 0), ':');
+            const sinkPrefix = sink_addr_iter.next() orelse return @intFromEnum(BofErrors.BadArgsProvided);
 
-            const host = iter2.next() orelse return 1;
-            const port = std.fmt.parseUnsigned(u16, iter2.next() orelse return 1, 10) catch return 1;
+            sinkAddrType = checkAddressType(sinkPrefix);
+            if (!(sinkAddrType == AddressType.TCP or sinkAddrType == AddressType.TLS))
+                return @intFromEnum(BofErrors.NotSupportedAddressType);
 
-            const tcp = std.net.tcpConnectToHost(allocator, host, port) catch return 1;
+            const host = sink_addr_iter.next() orelse return @intFromEnum(BofErrors.BadArgsProvided);
+            const port = std.fmt.parseUnsigned(u16, sink_addr_iter.next() orelse return 1, 10) catch return 1;
+
+            const tcp = std.net.tcpConnectToHost(allocator, host, port) catch return @intFromEnum(BofErrors.ConnectionError);
             defer tcp.close();
 
             var tcp_buf: [4096]u8 = undefined;
             var tcp_w = tcp.writer(&tcp_buf);
 
-            _ = file_r_iface.streamRemaining(&tcp_w.interface) catch return 1;
+            _ = file_r_iface.streamRemaining(&tcp_w.interface) catch @intFromEnum(BofErrors.DataTransferError);
         }
     }
 
