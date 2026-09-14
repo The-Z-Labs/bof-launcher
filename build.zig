9cc6bc21c4c24b6c90396e5f94815d7203aca75f
Michal Ziulek <michalziulek@gmail.com>
2026-09-14 14:40:01 +0200

    integration_with_c and some BOFs

--- build.zig
+++ build.zig
@@ -30,9 +30,85 @@ pub fn build(b: *std.Build) !void {
         "Prioritize performance, safety, or binary size (-O flag)",
     ) orelse .ReleaseSmall;
 
-    //const osTagStr = @import("bof_launcher_lib").osTagStr;
-    //const cpuArchStr = @import("bof_launcher_lib").cpuArchStr;
+    const osTagStr = @import("bof_launcher_lib").osTagStr;
+    const cpuArchStr = @import("bof_launcher_lib").cpuArchStr;
     const libFileName = @import("bof_launcher_lib").libFileName;
+
+    const has_python = blk: {
+        _ = b.findProgram(&.{"python"}, &.{}) catch break :blk false;
+        break :blk true;
+    };
+
+    //
+    // Install and post-process BOFs
+    //
+    const bofs_install_path = "bin/bofs/";
+    const bofs_dep = b.dependency("bof_launcher_bofs", .{ .optimize = optimize });
+    for (@import("bof_launcher_bofs").bofs_to_build) |bof| {
+        const full_name = bof.fullName(b.allocator);
+
+        var prev_step = &b.addInstallArtifact(
+            bofs_dep.artifact(full_name),
+            .{
+                .dest_dir = .{ .override = .{ .custom = bofs_install_path } },
+                .dest_sub_path = if (bof.optimize != .Debug) b.fmt("{s}.o", .{full_name}) else null,
+            },
+        ).step;
+
+        if (@import("builtin").cpu.arch == .x86_64) strip: {
+            // Always skip debug exe
+            if (std.mem.containsAtLeast(u8, full_name, 1, "debug")) break :strip;
+
+            // Blacklist (llvm-objcopy can't remove section for some reason)
+            if (std.mem.containsAtLeast(u8, full_name, 1, "udpScanner")) break :strip;
+            if (std.mem.containsAtLeast(u8, full_name, 1, "tcpScanner")) break :strip;
+            if (std.mem.containsAtLeast(u8, full_name, 1, "grep")) break :strip;
+            if (std.mem.containsAtLeast(u8, full_name, 1, "find")) break :strip;
+            if (std.mem.containsAtLeast(u8, full_name, 1, "socat")) break :strip;
+
+            if (std.mem.containsAtLeast(u8, full_name, 1, "coff")) {
+                const run = b.addSystemCommand(&.{
+                    "bin/llvm-objcopy",
+                    "--remove-section=.red",
+                    "--strip-unneeded",
+                    b.fmt("zig-out/" ++ bofs_install_path ++ "{s}.o", .{full_name}),
+                });
+                run.step.dependOn(prev_step);
+                prev_step = &run.step;
+            }
+        }
+
+        if (has_python) boflint: {
+            // Always skip debug exe
+            if (std.mem.containsAtLeast(u8, full_name, 1, "debug")) break :boflint;
+
+            // Blacklist (BOFs which use our custom bof*() API)
+            if (std.mem.containsAtLeast(u8, full_name, 1, "runBofFromBof")) break :boflint;
+            if (std.mem.containsAtLeast(u8, full_name, 1, "z-beac0n")) break :boflint;
+            if (std.mem.containsAtLeast(u8, full_name, 1, "wAsmTest")) break :boflint;
+
+            if (std.mem.containsAtLeast(u8, full_name, 1, "coff")) {
+                const run = b.addSystemCommand(&.{
+                    "python",
+                    "utils/boflint.py",
+                    "--logformat",
+                    "vs",
+                    "--loader",
+                    "cs",
+                    b.fmt("zig-out/" ++ bofs_install_path ++ "{s}.o", .{full_name}),
+                });
+                run.step.dependOn(prev_step);
+                prev_step = &run.step;
+            }
+        }
+
+        b.getInstallStep().dependOn(prev_step);
+    }
+
+    b.getInstallStep().dependOn(&b.addInstallFile(
+        bofs_dep.namedLazyPath("bof_collection_doc"),
+        "bof-collection.yaml",
+    ).step);
 
     //
     // Install bof launcher library
@@ -57,6 +133,25 @@ pub fn build(b: *std.Build) !void {
                 libFileName(b.allocator, target, "shared_nolibc"),
             ));
         }
+    }
+
+    for (supported_targets) |target_query| {
+        const target = b.resolveTargetQuery(target_query);
+
+        // integration with c
+        {
+            const dep = b.dependency("integration_with_c", .{ .target = target, .optimize = optimize });
+            const exe = dep.artifact(b.fmt(
+                "integration_with_c_{s}_{s}",
+                .{ osTagStr(target), cpuArchStr(target) },
+            ));
+            b.installArtifact(exe);
+        }
+
+        // bof stager
+        //const dep = b.dependency("bof_stager", .{ .target = target, .optimize = optimize });
+        //const exe = dep.artifact(b.fmt("bof_stager_{s}_{s}", .{ osTagStr(target), cpuArchStr(target) }));
+        //b.installArtifact(exe);
     }
 }
 
